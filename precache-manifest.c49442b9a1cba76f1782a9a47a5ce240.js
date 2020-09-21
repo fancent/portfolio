2f3f4abcfbc13c8e904344252a66feb39b24199e
fancent <vincent.kc.fan@gmail.com>
2020-09-22 02:17:49 +0800

    Updates

---
+++ precache-manifest.c49442b9a1cba76f1782a9a47a5ce240.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc5b5942496c342143510cccec70ece",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d90cb7deea26ee14dd39",
+    "url": "/portfolio/static/css/main.d0f1dbd3.chunk.css"
+  },
+  {
+    "revision": "1db948ce596954167e3f",
+    "url": "/portfolio/static/js/2.693b6c33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.693b6c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90cb7deea26ee14dd39",
+    "url": "/portfolio/static/js/main.c0d0efdb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "da3ec5aff27d148531c57e9b1063916b",
+    "url": "/portfolio/static/media/demo.da3ec5af.png"
+  },
+  {
+    "revision": "828a1e03fa4b981b820f4aa6d5b33247",
+    "url": "/portfolio/static/media/logo.828a1e03.png"
+  },
+  {
+    "revision": "2c90e253a5bc8afabaa17162d1cce0fe",
+    "url": "/portfolio/static/media/logo_selected.2c90e253.png"
+  },
+  {
+    "revision": "17e52f2bede568243fc2dd82f62c7674",
+    "url": "/portfolio/static/media/noise.17e52f2b.png"
+  }
+]);
